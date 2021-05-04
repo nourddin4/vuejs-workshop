8b0baa7664e204e833f6c3f10c83a69166961b9b
nourddin4 <76815899+nourddin4@users.noreply.github.com>
2021-05-04 11:17:36 +0000

    Update App.vue

--- signup/src/App.vue
+++ signup/src/App.vue
@@ -3,9 +3,6 @@
     <!-- Navigation -->
     <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
       <div class="container">
-        <!-- <a class="navbar-brand float-left" href="https://www.youcode.ma" target="_blank">
-           YouCode Youssoufia
-        </a> -->
         <ul class="nav navbar-nav flex-row float-right">
           <li class="nav-item">
             <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
